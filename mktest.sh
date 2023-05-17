db5332033700afaabd66478959bfd2c15aa7ae07
Matiboux <matiboux@gmail.com>
2023-05-17 16:19:00 +0200

    Add env files for tests

--- mktest.sh
+++ mktest.sh
@@ -8,5 +8,15 @@ touch ./test/docker-compose.override.yml
 touch ./test/docker-compose.prod.yml
 touch ./test/docker-compose.dev.yml
 
+touch ./test/.env
+touch ./test/.env.local
+touch ./test/.env.dev
+touch ./test/.env.dev.local
+touch ./test/.env.prod
+touch ./test/.env.prod.local
+
 touch ./test/docker-compose.top.yml
 touch ./test/docker/docker-compose.sub.yml
+
+touch ./test/.env.top
+touch ./test/docker/.env.sub
