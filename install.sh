6a46794ae48d6c7bb6918d18de64aab1300251a9
Mati <matiboux@gmail.com>
2023-06-23 15:58:20 +0200

    Add version argument to install script (#37)
    * Add version argument to install script
    
    * Pass dockerc version argument to install script
    
    * Update REQUIRED_TAG var name
    
    * Remove v from version passed to install script

--- install.sh
+++ install.sh
@@ -6,11 +6,19 @@
 # Copyright (c) 2023 Matiboux
 
 if [ "$#" -gt 0 ]; then
-	# Get install directory from first parameter and shift
+	# Get install directory from parameter and shift
 	INSTALL_DIR="$1"
 	shift
 else
 	INSTALL_DIR="/usr/local/bin"
+fi
+
+if [ "$#" -gt 0 ]; then
+	# Get specific version from parameter and shift
+	REQUIRED_TAG="v$1"
+	shift
+else
+	REQUIRED_TAG="HEAD"
 fi
 
 # Check that docker compose is installed
@@ -20,7 +28,7 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-curl -fsSL https://raw.githubusercontent.com/matiboux/dockerc/HEAD/dockerc -o "$INSTALL_DIR/dockerc"
+curl -fsSL https://raw.githubusercontent.com/matiboux/dockerc/$REQUIRED_TAG/dockerc -o "$INSTALL_DIR/dockerc"
 if [ $? -ne 0 ]; then
 	echo "Error: DockerC installation failed."
 	exit 1
@@ -32,4 +40,4 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-echo "DockerC installed successfully at '$INSTALL_DIR/dockerc'!"
+echo "DockerC ($REQUIRED_TAG) installed successfully at '$INSTALL_DIR/dockerc'!"
