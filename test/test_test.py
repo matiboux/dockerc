da099ab69312c74f645bd6afb655b69937b88a1a
Matiboux <matiboux@gmail.com>
2023-12-03 17:46:49 +0100

    Test file contents

--- test/test_test.py
+++ test/test_test.py
@@ -1,2 +1,8 @@
 def test_test():
     assert 3 + 2 == 5
+
+# Test the content of a file
+# Path: test/test_file.py
+def test_file():
+    with open('test/test_file.py') as f:
+        assert f.read() == 'def test_file():\n    assert True\n'
