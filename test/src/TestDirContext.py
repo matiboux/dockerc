4970184d2885862044c32d1361834b199f816348
Matiboux <matiboux@gmail.com>
2024-03-17 00:11:54 +0100

    Fix TestDirContext

--- test/src/TestDirContext.py
+++ test/src/TestDirContext.py
@@ -7,12 +7,16 @@ class TestDirContext(object):
     # Tell pytest to ignore this class
     __test__ = False
 
-    def __init__(self, file: str = __file__, test_name: str = None):
+    def __init__(
+        self,
+        file: str = __file__,
+        dockerc_path: str = None,
+    ):
         self.test_cwd = os.path.join(os.path.dirname(file), 'cwd')
-        self.dockerc_path = os.path.join(os.path.dirname(file), '..', '..', 'dockerc')
+        self.dockerc_path = dockerc_path or os.path.join(os.path.dirname(file), '..', '..', 'dockerc')
 
     def __enter__(self):
-        os.makedirs(self.dir_path, exist_ok = True)
+        os.makedirs(self.test_cwd, exist_ok = True)
         return self
 
     def __exit__(self, *args):
