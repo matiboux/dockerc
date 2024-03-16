c04e6df252d8dd4cba97b20298f3bd184dd93290
Matiboux <matiboux@gmail.com>
2024-03-16 23:29:22 +0100

    Revert "Try using WorkingDirTest in test_default"
    This reverts commit 33c5f67b37575ce69efda6c988ca15611ab83d9f.

--- test/test/none/test_default.py
+++ test/test/none/test_default.py
@@ -1,8 +1,7 @@
 from . import dir_files
-from test.src.WorkingDirTest import WorkingDirTest
-from test.src.assert_context_new import assert_context_not_found
+from test.src.reset_dir import reset_dir
+from test.src.assert_context import assert_context_not_found
 
 def test_default_not_found(dir_files = dir_files):
-    with WorkingDirTest('test_default_not_found') as twd:
-        twd.add_empty_files(dir_files)
-        assert_context_not_found(twd, None)
+    reset_dir('./twd', dir_files)
+    assert_context_not_found(None)
