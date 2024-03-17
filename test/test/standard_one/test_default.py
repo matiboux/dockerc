039363c36a59a68ced01cfafde1bd3ddca94c61d
Matiboux <matiboux@gmail.com>
2024-03-17 02:06:39 +0100

    Use new test method for standard one

--- test/test/standard_one/test_default.py
+++ test/test/standard_one/test_default.py
@@ -1,6 +1,21 @@
 from . import dir_files
 from test.src.reset_dir import reset_dir
 from test.src.assert_context import assert_context_found
+from test.src.format_dockerc_stdout import format_dockerc_stdout
+from test.src.TestDirContext import TestDirContext
+
+def test_default_new():
+    with TestDirContext(__file__) as ctx:
+        dockerc = ctx.run_dockerc(
+            None,
+        )
+        dockerc.assert_context_found(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' up -d'
+            ),
+        )
 
 def test_default(dir_files = dir_files):
     reset_dir('./twd', dir_files)
