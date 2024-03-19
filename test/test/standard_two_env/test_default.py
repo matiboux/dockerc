6f44fee2b7f56f6475bcc6dd13db66509f43269c
Matiboux <matiboux@gmail.com>
2024-03-19 23:54:31 +0100

    Add different groups for standard two with env files

---
+++ test/test/standard_two_env/test_default.py
@@ -0,0 +1,17 @@
+from test.src.format_dockerc_stdout import format_dockerc_stdout
+from test.src.TestDirContext import TestDirContext
+
+def test_default_env(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            None,
+        )
+        dockerc.assert_context_found(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' -f ./docker-compose.override.yml'
+                b' --env-file ./.env'
+                b' up -d'
+            ),
+        )
