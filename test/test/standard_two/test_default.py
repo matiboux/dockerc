bd1567e1ef63d7d659c2683152eb05e2b56b2fe8
Matiboux <matiboux@gmail.com>
2024-03-19 23:46:17 +0100

    Refactor standard two context tests

--- test/test/standard_two/test_default.py
+++ test/test/standard_two/test_default.py
@@ -1,62 +1,105 @@
-from . import dir_files
-from test.src.reset_dir import reset_dir
-from test.src.assert_context import assert_context_found
+from test.src.format_dockerc_stdout import format_dockerc_stdout
+from test.src.TestDirContext import TestDirContext
 
-def test_default(dir_files = dir_files):
-    reset_dir('./twd', dir_files)
-    assert_context_found(
-        None,
-        (
-            b'docker compose' \
-            b' -f ./docker-compose.yml' \
-            b' -f ./docker-compose.override.yml' \
-            b' up -d'
-        ),
-    )
+def test_default(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            None,
+        )
+        dockerc.assert_context_found(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' -f ./docker-compose.override.yml'
+                b' up -d'
+            ),
+        )
 
-def test_default_env(dir_files = dir_files):
-    reset_dir('./twd', dir_files + [
-        '.env',
-    ])
-    assert_context_found(
-        None,
-        (
-            b'docker compose' \
-            b' -f ./docker-compose.yml' \
-            b' -f ./docker-compose.override.yml' \
-            b' --env-file ./.env' \
-            b' up -d'
-        ),
-    )
+def test_dev_not_found(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            'dev',
+        )
+        dockerc.assert_context_found(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' -f ./docker-compose.override.yml'
+                b' up -d'
+            ),
+        )
 
-def test_default_env_local(dir_files = dir_files):
-    reset_dir('./twd', dir_files + [
-        '.env.local',
-    ])
-    assert_context_found(
-        None,
-        (
-            b'docker compose' \
-            b' -f ./docker-compose.yml' \
-            b' -f ./docker-compose.override.yml' \
-            b' --env-file ./.env.local' \
-            b' up -d'
-        ),
-    )
+def test_override_not_found(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            'override',
+        )
+        dockerc.assert_context_found(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' -f ./docker-compose.override.yml'
+                b' up -d'
+            ),
+        )
 
-def test_default_env_both():
-    reset_dir('./twd', dir_files + [
-        '.env',
-        '.env.local',
-    ])
-    assert_context_found(
-        None,
-        (
-            b'docker compose' \
-            b' -f ./docker-compose.yml' \
-            b' -f ./docker-compose.override.yml' \
-            b' --env-file ./.env' \
-            b' --env-file ./.env.local' \
-            b' up -d'
-        ),
-    )
+def test_prod_not_found(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            'prod',
+        )
+        dockerc.assert_context_not_found()
+
+def test_what_not_found(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            'what',
+        )
+        dockerc.assert_context_not_found()
+
+# def test_default_env(dir_files = dir_files):
+#     reset_dir('./twd', dir_files + [
+#         '.env',
+#     ])
+#     assert_context_found(
+#         None,
+#         (
+#             b'docker compose' \
+#             b' -f ./docker-compose.yml' \
+#             b' -f ./docker-compose.override.yml' \
+#             b' --env-file ./.env' \
+#             b' up -d'
+#         ),
+#     )
+
+# def test_default_env_local(dir_files = dir_files):
+#     reset_dir('./twd', dir_files + [
+#         '.env.local',
+#     ])
+#     assert_context_found(
+#         None,
+#         (
+#             b'docker compose' \
+#             b' -f ./docker-compose.yml' \
+#             b' -f ./docker-compose.override.yml' \
+#             b' --env-file ./.env.local' \
+#             b' up -d'
+#         ),
+#     )
+
+# def test_default_env_both():
+#     reset_dir('./twd', dir_files + [
+#         '.env',
+#         '.env.local',
+#     ])
+#     assert_context_found(
+#         None,
+#         (
+#             b'docker compose' \
+#             b' -f ./docker-compose.yml' \
+#             b' -f ./docker-compose.override.yml' \
+#             b' --env-file ./.env' \
+#             b' --env-file ./.env.local' \
+#             b' up -d'
+#         ),
+#     )
