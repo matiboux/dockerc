d46ee4a908165f4b2f1a929b2a0a30efea61cd9e
Matiboux <matiboux@gmail.com>
2024-03-20 00:29:35 +0100

    Add imported tests in base_prod & base_prod_sub groups

---
+++ test/test/base_prod/test_import.py
@@ -0,0 +1,4 @@
+from test.test.empty.test_base import test_base_what_not_found as func_test_base_what_not_found
+
+def test_base_what_not_found(file = __file__):
+	func_test_base_what_not_found(file)
