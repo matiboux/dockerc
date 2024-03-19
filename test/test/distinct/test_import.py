e8635ca4af4462f1896c3359c71e6b6e8206b8f9
Matiboux <matiboux@gmail.com>
2024-03-20 00:07:34 +0100

    Update other imported tests

--- test/test/distinct/test_import.py
+++ test/test/distinct/test_import.py
@@ -1,9 +1,8 @@
-from . import dir_files
-from test.test.none.test_distinct_prod import test_distinct_prod as func_test_distinct_prod
-from test.test.none.test_distinct_what import test_distinct_what as func_test_distinct_what
+from test.test.empty.test_base import test_base_prod_not_found as func_test_distinct_prod
+from test.test.empty.test_base import test_base_what_not_found as func_test_distinct_what
 
-def test_distinct_prod(dir_files = dir_files):
-	func_test_distinct_prod(dir_files)
+def test_distinct_prod(file = __file__):
+	func_test_distinct_prod(file)
 
-def test_distinct_what(dir_files = dir_files):
-	func_test_distinct_what(dir_files)
+def test_distinct_what(file = __file__):
+	func_test_distinct_what(file)
