a070d86b08202c238b14efefc9665fc383281057
Matiboux <matiboux@gmail.com>
2024-03-19 22:53:39 +0100

    Refactor none context tests

--- test/test/none/test_base.py
+++ test/test/none/test_base.py
@@ -1,36 +1,36 @@
 from test.src.TestDirContext import TestDirContext
 
-def test_default_not_found():
+def test_base_not_found():
     with TestDirContext(__file__) as ctx:
         dockerc = ctx.run_dockerc(
-            None,
+            'base',
         )
         dockerc.assert_context_not_found()
 
-def test_dev_not_found():
+def test_base_dev_not_found():
     with TestDirContext(__file__) as ctx:
         dockerc = ctx.run_dockerc(
-            'dev',
+            'base.dev',
         )
         dockerc.assert_context_not_found()
 
-def test_override_not_found():
+def test_base_override_not_found():
     with TestDirContext(__file__) as ctx:
         dockerc = ctx.run_dockerc(
-            'override',
+            'base.override',
         )
         dockerc.assert_context_not_found()
 
-def test_prod_not_found():
+def test_base_prod_not_found():
     with TestDirContext(__file__) as ctx:
         dockerc = ctx.run_dockerc(
-            'prod',
+            'base.prod',
         )
         dockerc.assert_context_not_found()
 
-def test_what_not_found():
+def test_base_what_not_found():
     with TestDirContext(__file__) as ctx:
         dockerc = ctx.run_dockerc(
-            'what',
+            'base.what',
         )
         dockerc.assert_context_not_found()
--- test/test/none/test_dev.py
+++
@@ -1,7 +0,0 @@
-from . import dir_files
-from test.src.reset_dir import reset_dir
-from test.src.assert_context import assert_context_not_found
-
-def test_dev_not_found(dir_files = dir_files):
-    reset_dir('./twd', dir_files)
-    assert_context_not_found('dev')
