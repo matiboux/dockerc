843ecc5338c004c419e1712c4e70d7699349081e
Matiboux <matiboux@gmail.com>
2024-03-20 00:29:52 +0100

    Remove distinct tests group

--- test/test/distinct/test_import.py
+++
@@ -1,8 +0,0 @@
-from test.test.empty.test_base import test_base_prod_not_found as func_test_distinct_prod
-from test.test.empty.test_base import test_base_what_not_found as func_test_distinct_what
-
-def test_distinct_prod(file = __file__):
-	func_test_distinct_prod(file)
-
-def test_distinct_what(file = __file__):
-	func_test_distinct_what(file)
--- test/test/distinct_level_two/__init__.py
+++
@@ -1,5 +0,0 @@
-dir_files = [
-	'docker-compose-distinct.yml',
-	'docker-compose-distinct.prod.yml',
-	'docker-compose-distinct.prod.task.yml',
-]
