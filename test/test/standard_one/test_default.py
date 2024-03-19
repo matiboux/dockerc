b87584b57113dc1532c823e9611cfb8aacde373c
Matiboux <matiboux@gmail.com>
2024-03-19 23:16:12 +0100

    Refactor standard one context tests

--- test/test/standard_one/test_default.py
+++ test/test/standard_one/test_default.py
@@ -1,6 +1,3 @@
-from . import dir_files
-from test.src.reset_dir import reset_dir
-from test.src.assert_context import assert_context_found
 from test.src.format_dockerc_stdout import format_dockerc_stdout
 from test.src.TestDirContext import TestDirContext
 
@@ -16,3 +13,37 @@ def test_default():
                 b' up -d'
             ),
         )
+
+def test_dev_not_found():
+    with TestDirContext(__file__) as ctx:
+        dockerc = ctx.run_dockerc(
+            'dev',
+        )
+        dockerc.assert_context_not_found()
+
+def test_override_not_found():
+    with TestDirContext(__file__) as ctx:
+        dockerc = ctx.run_dockerc(
+            'override',
+        )
+        dockerc.assert_context_not_found()
+
+def test_prod_not_found():
+    with TestDirContext(__file__) as ctx:
+        dockerc = ctx.run_dockerc(
+            'prod',
+        )
+        dockerc.assert_context_found(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' up -d'
+            ),
+        )
+
+def test_what_not_found():
+    with TestDirContext(__file__) as ctx:
+        dockerc = ctx.run_dockerc(
+            'what',
+        )
+        dockerc.assert_context_not_found()
