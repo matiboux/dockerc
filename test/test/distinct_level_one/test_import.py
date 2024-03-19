1c66fffee4481993263c846bf87e9db03867c1aa
Matiboux <matiboux@gmail.com>
2024-03-20 00:15:22 +0100

    Fix old imported tests

--- test/test/distinct_level_one/test_import.py
+++ test/test/distinct_level_one/test_import.py
@@ -1,8 +1,9 @@
+from . import dir_files
 from test.test.distinct.test_distinct import test_distinct as func_test_distinct
 from test.test.empty.test_base import test_base_what_not_found as func_test_distinct_what
 
-def test_distinct(file = __file__):
-	func_test_distinct(file)
+def test_distinct(dir_files = dir_files):
+	func_test_distinct(dir_files)
 
 def test_distinct_what(file = __file__):
 	func_test_distinct_what(file)
