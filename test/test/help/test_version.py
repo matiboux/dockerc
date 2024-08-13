ef6eaea6489eb98ffa5eb236213c27c3231c69b5
Matiboux <matiboux@gmail.com>
2024-08-13 23:35:05 +0200

    Add version dry run test

--- test/test/help/test_version.py
+++ test/test/help/test_version.py
@@ -17,3 +17,17 @@ def test_version_shorthand(file = __file__):
             '-v',
         )
         dockerc.assert_context_found(VERSION_STDOUT_SHORT)
+
+def test_version_dry(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-n', '--version',
+        )
+        dockerc.assert_context_found(VERSION_STDOUT_SHORT)
+
+def test_version_dry_shorthand(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-n', '-v',
+        )
+        dockerc.assert_context_found(VERSION_STDOUT_SHORT)
