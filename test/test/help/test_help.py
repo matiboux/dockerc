3cd2434dc1357169caf87d13faca1c7f8123c5e0
Matiboux <matiboux@gmail.com>
2024-08-13 22:40:50 +0200

    Update help tests

--- test/test/help/test_help.py
+++ test/test/help/test_help.py
@@ -2,45 +2,40 @@ import subprocess
 
 from test.src.reset_dir import reset_dir
 
-HELP_STDOUT = (
-    b'Usage: ../dockerc [options] [context] [...args]\n'
-    b'  args: Arguments passed to docker compose\n'
-    b'  context syntax: [first] | [first]-[second] | "-" | "--"\n'
-    b'    first   First part of the context\n'
-    b'    second  Second part of the context\n'
-    b'    "-"     Use default docker compose files ("override" if it exists)\n'
-    b'    "--"    Use docker compose without file arguments\n'
-    b'  options:\n'
-    b'    -h, --help     Print this help and exit\n'
-    b'    -v, --version  Print version and exit\n'
-    b'    --update       Update DockerC and exit\n'
-    b'    -n             Dry run, print docker compose command without running it\n'
-    b'    -q             Quiet, do not print docker compose command\n'
-)
-
-def test_help():
-    reset_dir('./twd')
-    proc = subprocess.Popen(
-        ['../dockerc', '--help'],
-        cwd = './twd',
-        stdout = subprocess.PIPE,
+def get_help_stdout(dockerc_path: str):
+    return (
+        b'Usage: ' + dockerc_path.encode() + b' [options] [context] [...args]\n'
+        b'  args: Arguments passed to docker compose\n'
+        b'  context syntax: [first] | [first]-[second] | "-" | "--"\n'
+        b'    first   First part of the context\n'
+        b'    second  Second part of the context\n'
+        b'    "-"     Use default docker compose files ("override" if it exists)\n'
+        b'    "--"    Use docker compose without file arguments\n'
+        b'  options:\n'
+        b'    -h, --help     Print this help and exit\n'
+        b'    -v, --version  Print version and exit\n'
+        b'    --update       Update DockerC and exit\n'
+        b'    -n             Dry run, print docker compose command without running it\n'
+        b'    -q             Quiet, do not print docker compose command\n'
     )
-    stdout, stderr = proc.communicate()
-    assert stdout == HELP_STDOUT
-    assert stderr == None
-    assert proc.returncode == 0
 
-def test_help_shorthand():
-    reset_dir('./twd')
-    proc = subprocess.Popen(
-        ['../dockerc', '-h'],
-        cwd = './twd',
-        stdout = subprocess.PIPE,
-    )
-    stdout, stderr = proc.communicate()
-    assert stdout == HELP_STDOUT
-    assert stderr == None
-    assert proc.returncode == 0
+def test_help(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '--help',
+        )
+        dockerc.assert_context_found(
+            get_help_stdout(dockerc.dockerc_path),
+        )
+
+def test_help_shorthand(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-h',
+        )
+        dockerc.assert_context_found(
+            get_help_stdout(dockerc.dockerc_path),
+        )
 
 def test_docker_presets_help():
     reset_dir('./twd')
