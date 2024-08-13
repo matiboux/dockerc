78b00bbce16dbceae1c709f8490e46126c6dcc28
Matiboux <matiboux@gmail.com>
2024-08-13 22:48:25 +0200

    Refactor help tests

--- test/test/help_args/test_help.py
+++ test/test/help_args/test_help.py
@@ -1,40 +1,43 @@
 from test.src.TestDirContext import TestDirContext
 
+def get_help_stdout(dockerc_path: str):
+    return (
+        b'Usage: ' + dockerc_path.encode() + b' [options] [context] [@preset] [...args]\n'
+        b'  args: Arguments passed to docker compose\n'
+        b'  @preset:\n'
+        b'    @u     up -d\n'
+        b'    @ub    up --build -d\n'
+        b'    @ubf   up --build -d --force-recreate\n'
+        b'    @ubfp  up --build -d --force-recreate --pull always\n'
+        b'    @ubr   up --build -d --remove-orphans\n'
+        b'    @ubfr  up --build -d --force-recreate --remove-orphans\n'
+        b'    @uf    up -d --force-recreate\n'
+        b'    @ur    up -d --remove-orphans\n'
+        b'    @ufr   up -d --force-recreate --remove-orphans\n'
+        b'    @d     down --remove-orphans\n'
+        b'    @dr    down --remove-orphans --rmi local\n'
+        b'    @dra   down --remove-orphans --rmi all\n'
+        b'    @drav  down --remove-orphans --rmi all -v\n'
+        b'    @r     run --rm\n'
+        b'    @ri    run --rm -it\n'
+        b'    @l     logs\n'
+        b'    @rf    rm -f\n'
+        b'    @rfv   rm -f -v\n'
+        b'  context syntax: [first] | [first]-[second] | "-" | "--"\n'
+        b'    first   First part of the context\n'
+        b'    second  Second part of the context\n'
+        b'    "-"     Use default docker compose files ("override" if it exists)\n'
+        b'    "--"    Use docker compose without file arguments\n'
+        b'  options:\n'
+        b'    -n  Dry run, print docker compose command without running it\n'
+        b'    -q  Quiet, do not print docker compose command\n'
+    )
+
 def test_help_args_presets(file = __file__):
     with TestDirContext(file) as ctx:
         dockerc = ctx.run_dockerc(
             '-', '@',
         )
         dockerc.assert_context_found(
-            (
-                b'Usage: ' + dockerc.dockerc_path.encode() + b' [options] [context] [@preset] [...args]\n'
-                b'  args: Arguments passed to docker compose\n'
-                b'  @preset:\n'
-                b'    @u     up -d\n'
-                b'    @ub    up --build -d\n'
-                b'    @ubf   up --build -d --force-recreate\n'
-                b'    @ubfp  up --build -d --force-recreate --pull always\n'
-                b'    @ubr   up --build -d --remove-orphans\n'
-                b'    @ubfr  up --build -d --force-recreate --remove-orphans\n'
-                b'    @uf    up -d --force-recreate\n'
-                b'    @ur    up -d --remove-orphans\n'
-                b'    @ufr   up -d --force-recreate --remove-orphans\n'
-                b'    @d     down --remove-orphans\n'
-                b'    @dr    down --remove-orphans --rmi local\n'
-                b'    @dra   down --remove-orphans --rmi all\n'
-                b'    @drav  down --remove-orphans --rmi all -v\n'
-                b'    @r     run --rm\n'
-                b'    @ri    run --rm -it\n'
-                b'    @l     logs\n'
-                b'    @rf    rm -f\n'
-                b'    @rfv   rm -f -v\n'
-                b'  context syntax: [first] | [first]-[second] | "-" | "--"\n'
-                b'    first   First part of the context\n'
-                b'    second  Second part of the context\n'
-                b'    "-"     Use default docker compose files ("override" if it exists)\n'
-                b'    "--"    Use docker compose without file arguments\n'
-                b'  options:\n'
-                b'    -n  Dry run, print docker compose command without running it\n'
-                b'    -q  Quiet, do not print docker compose command\n'
-            ),
+            get_help_stdout(dockerc.dockerc_path),
         )
--- test/test/help_docker/test_help.py
+++ test/test/help_docker/test_help.py
@@ -1,24 +1,27 @@
 from test.src.TestDirContext import TestDirContext
 
+def get_help_stdout(dockerc_path: str):
+    return (
+        b'Usage: ' + dockerc_path.encode() + b' [options] [@preset] [...args]\n'
+        b'  args: Arguments passed to docker\n'
+        b'  @preset:\n'
+        b'    @rfc   Remove unused containers\n'
+        b'    @rfca  Remove all unused containers\n'
+        b'    @rfi   Remove unused images\n'
+        b'    @rfia  Remove all unused images\n'
+        b'    @rf    Remove unused containers, networks and images\n'
+        b'    @rfa   Remove all unused containers, networks and images\n'
+        b'    @rfav  Remove all unused containers, networks, images and volumes\n'
+        b'  options:\n'
+        b'    -n  Dry run, print docker command without running it\n'
+        b'    -q  Quiet, do not print docker command\n'
+    )
+
 def test_help_docker_presets(file = __file__):
     with TestDirContext(file) as ctx:
         dockerc = ctx.run_dockerc(
             '@',
         )
         dockerc.assert_context_found(
-            (
-                b'Usage: ' + dockerc.dockerc_path.encode() + b' [options] [@preset] [...args]\n'
-                b'  args: Arguments passed to docker\n'
-                b'  @preset:\n'
-                b'    @rfc   Remove unused containers\n'
-                b'    @rfca  Remove all unused containers\n'
-                b'    @rfi   Remove unused images\n'
-                b'    @rfia  Remove all unused images\n'
-                b'    @rf    Remove unused containers, networks and images\n'
-                b'    @rfa   Remove all unused containers, networks and images\n'
-                b'    @rfav  Remove all unused containers, networks, images and volumes\n'
-                b'  options:\n'
-                b'    -n  Dry run, print docker command without running it\n'
-                b'    -q  Quiet, do not print docker command\n'
-            ),
+            get_help_stdout(dockerc.dockerc_path),
         )
