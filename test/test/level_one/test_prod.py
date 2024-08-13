340a4b2d62c389fa81921393d5b332146a7ecf53
Matiboux <matiboux@gmail.com>
2024-08-13 23:40:14 +0200

    Update prod simple test

--- test/test/level_one/test_prod.py
+++ test/test/level_one/test_prod.py
@@ -1,14 +1,15 @@
-from . import dir_files
-from test.src.reset_dir import reset_dir
-from test.src.assert_context import assert_context_found
+from test.src.format_dockerc_stdout import format_dockerc_stdout
+from test.src.TestDirContext import TestDirContext
 
-def test_prod_simple(dir_files = dir_files):
-    reset_dir('./twd', dir_files)
-    assert_context_found(
-        'prod',
-        (
-            b'docker compose' \
-            b' -f ./docker-compose.yml -f ./docker-compose.prod.yml' \
-            b' up -d'
-        ),
-    )
+def test_prod_simple(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            'prod',
+        )
+        dockerc.assert_context_found(
+            format_dockerc_stdout(
+                b'docker compose' \
+                b' -f ./docker-compose.yml -f ./docker-compose.prod.yml' \
+                b' up -d'
+            ),
+        )
