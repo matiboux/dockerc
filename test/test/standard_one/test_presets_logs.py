567cdcb2a651bf074720a0bd343f83b4662bd556
Matiboux <matiboux@gmail.com>
2024-08-15 00:29:08 +0200

    Add logs args presets tests

---
+++ test/test/standard_one/test_presets_logs.py
@@ -0,0 +1,210 @@
+from test.src.format_dockerc_stdout import format_dockerc_stdout
+from test.src.TestDirContext import TestDirContext
+
+def test_presets_l(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@l',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' logs -n 100'
+            )
+        )
+
+def test_presets_lf(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@lf',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' logs -n 100 -f'
+            )
+        )
+
+def test_presets_lt(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@lt',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' logs -n 100 -t'
+            )
+        )
+
+def test_presets_ls(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@ls',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' logs -n 100 --no-log-prefix'
+            )
+        )
+
+def test_presets_lft(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@lft',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' logs -n 100 -f -t'
+            )
+        )
+
+def test_presets_lfs(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@lfs',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' logs -n 100 -f --no-log-prefix'
+            )
+        )
+
+def test_presets_lfts(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@lfts',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' logs -n 100 -f -t --no-log-prefix'
+            )
+        )
+
+def test_presets_lts(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@lts',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' logs -n 100 -t --no-log-prefix'
+            )
+        )
+
+def test_presets_ll(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@ll',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' logs'
+            )
+        )
+
+def test_presets_llf(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@llf',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' logs -f'
+            )
+        )
+
+def test_presets_llt(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@llt',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' logs -t'
+            )
+        )
+
+def test_presets_lls(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@lls',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' logs --no-log-prefix'
+            )
+        )
+
+def test_presets_llft(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@llft',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' logs -f -t'
+            )
+        )
+
+def test_presets_llfs(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@llfs',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' logs -f --no-log-prefix'
+            )
+        )
+
+def test_presets_llfts(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@llfts',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' logs -f -t --no-log-prefix'
+            )
+        )
+
+def test_presets_llts(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@llts',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' logs -t --no-log-prefix'
+            )
+        )
