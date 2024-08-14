ba162c6941457c57c7b6b69468fd9c1fe341514b
Matiboux <matiboux@gmail.com>
2024-08-15 00:21:47 +0200

    Add run args presets tests

---
+++ test/test/standard_one/test_presets_run.py
@@ -0,0 +1,184 @@
+from test.src.format_dockerc_stdout import format_dockerc_stdout
+from test.src.TestDirContext import TestDirContext
+
+def test_presets_r(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@r',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' run --rm'
+            )
+       )
+
+def test_presets_rw(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@rw',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' run -w /pwd -v ' + dockerc.cwd.encode() + b':/pwd --rm'
+            )
+        )
+
+def test_presets_rd(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@rd',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' run -d'
+            )
+        )
+
+def test_presets_rk(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@rk',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' run'
+            )
+        )
+
+def test_presets_rb(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@rb',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' run --build --rm'
+            )
+        )
+
+def test_presets_rbw(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@rbw',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' run --build -w /pwd -v ' + dockerc.cwd.encode() + b':/pwd --rm'
+            )
+        )
+
+def test_presets_rbd(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@rbd',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' run --build -d'
+            )
+        )
+
+def test_presets_rbk(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@rbk',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' run --build'
+            )
+        )
+
+def test_presets_ri(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@ri',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' run -i --rm'
+            )
+        )
+
+def test_presets_rwi(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@rwi',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' run -w /pwd -v ' + dockerc.cwd.encode() + b':/pwd -i --rm'
+            )
+        )
+
+def test_presets_rki(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@rki',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' run -i'
+            )
+        )
+
+def test_presets_rbi(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@rbi',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' run --build -i --rm'
+            )
+        )
+
+def test_presets_rbwi(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@rbwi',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' run --build -w /pwd -v ' + dockerc.cwd.encode() + b':/pwd -i --rm'
+            )
+        )
+
+def test_presets_rbki(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-', '@rbki',
+        )
+        dockerc.assert_context(
+            format_dockerc_stdout(
+                b'docker compose'
+                b' -f ./docker-compose.yml'
+                b' run --build -i'
+            )
+        )
