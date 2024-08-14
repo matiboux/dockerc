57cdbd18e244d39c7f91879ffad3c64c9fb9d9ee
Matiboux <matiboux@gmail.com>
2024-08-15 00:39:28 +0200

    Add quiet option test

---
+++ test/test/standard_one/test_quiet.py
@@ -0,0 +1,9 @@
+from test.src.format_dockerc_stdout import format_dockerc_stdout
+from test.src.TestDirContext import TestDirContext
+
+def test_quiet(file = __file__):
+    with TestDirContext(file) as ctx:
+        dockerc = ctx.run_dockerc(
+            '-q',
+        )
+        dockerc.assert_context_found()
