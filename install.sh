4633ef144b8781269af723b862f06bc745834fdf
Matiboux <matiboux@gmail.com>
2024-11-13 22:18:34 +0100

    Refactor quotes in install script

--- install.sh
+++ install.sh
@@ -9,20 +9,20 @@
 
 if [ "$#" -gt 0 ]; then
 
-	if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
-		echo "Usage: install.sh [install_dir] [version]"
-		echo "  install_dir: Installation directory (default: /usr/local/bin)"
-		echo "  version: Specific version to install (default: HEAD)"
+	if [ "$1" = '--help' ] || [ "$1" = '-h' ]; then
+		echo "Usage: $0 [install_dir] [version]"
+		echo '  install_dir: Installation directory (default: /usr/local/bin)'
+		echo '  version: Specific version to install (default: HEAD)'
 		exit 0
 	fi
 
-	if [ "$1" = "--install-dir" ] || [ "$1" = "-i" ]; then
+	if [ "$1" = '--install-dir' ] || [ "$1" = '-i' ]; then
 		# Installation directory argument is provided
 		shift
 		DOCKERC_INSTALL_DIR="$1"
 		shift
 		if [ -z "$DOCKERC_INSTALL_DIR" ]; then
-			echo "Error: Missing installation directory." >&2
+			echo 'Error: Missing installation directory.' >&2
 			exit 1
 		fi
 	fi
@@ -50,32 +50,32 @@ if [ -n "$DOCKERC_REQUIRED_TAG" ]; then
 	REQUIRED_TAG="$DOCKERC_REQUIRED_TAG"
 else
 	# Default required tag
-	REQUIRED_TAG="HEAD"
+	REQUIRED_TAG='HEAD'
 fi
 
 # Check that docker is installed
 docker --help > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-	echo "Error: Docker is not installed." >&2
+	echo 'Error: Docker is not installed.' >&2
 	exit 1
 fi
 
 # Check that docker compose is installed
 docker compose --help > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-	echo "Error: Docker compose is not installed." >&2
+	echo 'Error: Docker compose is not installed.' >&2
 	exit 1
 fi
 
 curl -fsSL https://raw.githubusercontent.com/matiboux/dockerc/$REQUIRED_TAG/dockerc -o "$INSTALL_DIR/dockerc"
 if [ $? -ne 0 ]; then
-	echo "Error: DockerC installation failed." >&2
+	echo 'Error: DockerC installation failed.' >&2
 	exit 1
 fi
 
 chmod +x "$INSTALL_DIR/dockerc"
 if [ $? -ne 0 ]; then
-	echo "Error: Failed to add execute permission." >&2
+	echo 'Error: Failed to add execute permission.' >&2
 	exit 1
 fi
 
