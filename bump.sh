ea956a83db9cd88ef5252308aa251fc59f87d421
Matiboux <matiboux@gmail.com>
2024-11-13 22:05:21 +0100

    Fix sed commands in bump script

--- bump.sh
+++ bump.sh
@@ -39,11 +39,11 @@ fi
 if [ "$(uname)" = "Darwin" ]; then
 	# MacOS
 	sed -i "" "3 s/\# DockerC.*/\# DockerC (v$VERSION)/g" dockerc
-	sed -i "" "9 s/VERSION=.*/VERSION=\"$VERSION\"/g" dockerc
+	sed -i "" "10 s/VERSION=.*/VERSION=\"$VERSION\"/g" dockerc
 else
 	# Linux
 	sed -i "3 s/\# DockerC.*/\# DockerC (v$VERSION)/g" dockerc
-	sed -i "9 s/VERSION=.*/VERSION=\"$VERSION\"/g" dockerc
+	sed -i "10 s/VERSION=.*/VERSION=\"$VERSION\"/g" dockerc
 fi
 
 if [ "$HAS_GIT" = true ]; then
