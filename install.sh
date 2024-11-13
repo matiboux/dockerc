a4923b33f8e5761fe5ad26cfd5801232211d6259
Matiboux <matiboux@gmail.com>
2024-11-13 17:39:04 +0100

    Print errors to stderr

--- install.sh
+++ install.sh
@@ -24,19 +24,19 @@ fi
 # Check that docker compose is installed
 docker compose --help > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-	echo "Error: Docker compose is not installed."
+	echo "Error: Docker compose is not installed." >&2
 	exit 1
 fi
 
 curl -fsSL https://raw.githubusercontent.com/matiboux/dockerc/$REQUIRED_TAG/dockerc -o "$INSTALL_DIR/dockerc"
 if [ $? -ne 0 ]; then
-	echo "Error: DockerC installation failed."
+	echo "Error: DockerC installation failed." >&2
 	exit 1
 fi
 
 chmod +x "$INSTALL_DIR/dockerc"
 if [ $? -ne 0 ]; then
-	echo "Error: Failed to add execute permission."
+	echo "Error: Failed to add execute permission." >&2
 	exit 1
 fi
 
