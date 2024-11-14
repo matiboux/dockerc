86a0fa9406e9048d2ddd27378ef5174129bdecc7
Mati <matiboux@gmail.com>
2024-11-14 01:57:28 +0100

    Refactor install script (#83)

--- install.sh
+++ install.sh
@@ -7,76 +7,100 @@
 # Copyright (c) 2023 Matiboux
 # This project is not affiliated with Docker, Inc.
 
-if [ "$#" -gt 0 ]; then
+ERROR_CODE=''
+DOCKERC_PARSE_ARGUMENTS='true'
 
-	if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
-		echo "Usage: install.sh [install_dir] [version]"
-		echo "  install_dir: Installation directory (default: /usr/local/bin)"
-		echo "  version: Specific version to install (default: HEAD)"
-		exit 0
-	fi
+# Parse options arguments
+while [ "$DOCKERC_PARSE_ARGUMENTS" = 'true' ] && [ "$#" -gt 0 ]; do
 
-	if [ "$1" = "--install-dir" ] || [ "$1" = "-i" ]; then
+	if [ "$1" = '--help' ] || [ "$1" = '-h' ]; then
+		DOCKERC_PRINT_HELP='true'
+		shift
+
+	elif [ "$1" = '--install-dir' ] || [ "$1" = '-i' ]; then
 		# Installation directory argument is provided
 		shift
+		if [ -z "$1" ]; then
+			echo 'Error: Missing installation directory.' >&2
+			DOCKERC_PRINT_HELP='true'
+			ERROR_CODE=1
+			# Stop parsing arguments
+			DOCKERC_PARSE_ARGUMENTS='false'
+			break
+		fi
 		DOCKERC_INSTALL_DIR="$1"
 		shift
-		if [ -z "$DOCKERC_INSTALL_DIR" ]; then
-			echo "Error: Missing installation directory." >&2
-			exit 1
-		fi
+
+	else
+		# Unknown option, maybe first argument
+		# Stop parsing options
+		break
 	fi
 
-	if [ -n "$1" ]; then
-		# Version argument is provided
-		DOCKERC_REQUIRED_TAG="v$1"
+done
+
+if [ "$DOCKERC_PARSE_ARGUMENTS" = 'true' ]; then
+	# Parse positional arguments
+
+	# Parse install tag optional argument
+	if [ "$#" -gt 0 ]; then
+		DOCKERC_INSTALL_TAG="$1"
 		shift
 	fi
 
+fi
+
+if [ "$DOCKERC_PRINT_HELP" = 'true' ]; then
+	# Print help & exit
+	echo "Usage: $0 [options] [tag]"
+	echo ''
+	echo 'Options:'
+	echo '  --help, -h         Display this help message'
+	echo '  --install-dir, -i  Installation directory (defaults to /usr/local/bin)'
+	echo ''
+	echo 'Arguments:'
+	echo '  tag  DockerC tag/version to install (defaults to HEAD)'
+	exit ${ERROR_CODE:-0}
 fi
 
 # Get installation directory
+INSTALL_DIR='/usr/local/bin' # Default installation directory
 if [ -n "$DOCKERC_INSTALL_DIR" ]; then
 	# Use from argument or environment variable
 	INSTALL_DIR="$DOCKERC_INSTALL_DIR"
-else
-	# Default installation directory
-	INSTALL_DIR='/usr/local/bin'
 fi
 
-# Get required tag to install
-if [ -n "$DOCKERC_REQUIRED_TAG" ]; then
+# Get install tag
+INSTALL_TAG='HEAD' # Default required tag
+if [ -n "$DOCKERC_INSTALL_TAG" ]; then
 	# Use from argument or environment variable
-	REQUIRED_TAG="$DOCKERC_REQUIRED_TAG"
-else
-	# Default required tag
-	REQUIRED_TAG="HEAD"
+	INSTALL_TAG="$DOCKERC_INSTALL_TAG"
 fi
 
 # Check that docker is installed
 docker --help > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-	echo "Error: Docker is not installed." >&2
+	echo 'Error: Docker is not installed.' >&2
 	exit 1
 fi
 
 # Check that docker compose is installed
 docker compose --help > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-	echo "Error: Docker compose is not installed." >&2
+	echo 'Error: Docker compose is not installed.' >&2
 	exit 1
 fi
 
-curl -fsSL https://raw.githubusercontent.com/matiboux/dockerc/$REQUIRED_TAG/dockerc -o "$INSTALL_DIR/dockerc"
+curl -fsSL "https://raw.githubusercontent.com/matiboux/dockerc/$INSTALL_TAG/dockerc" -o "$INSTALL_DIR/dockerc"
 if [ $? -ne 0 ]; then
-	echo "Error: DockerC installation failed." >&2
+	echo 'Error: DockerC installation failed.' >&2
 	exit 1
 fi
 
 chmod +x "$INSTALL_DIR/dockerc"
 if [ $? -ne 0 ]; then
-	echo "Error: Failed to add execute permission." >&2
+	echo 'Error: Failed to add execute permission.' >&2
 	exit 1
 fi
 
-echo "DockerC ($REQUIRED_TAG) installed successfully at '$INSTALL_DIR/dockerc'!"
+echo "DockerC ($INSTALL_TAG) installed successfully at '$INSTALL_DIR/dockerc'!"
