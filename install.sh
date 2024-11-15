6f6c256e53316fcbdf81b5d6b5bb9b6d0b4de35c
Matiboux <matiboux@gmail.com>
2024-11-16 00:06:43 +0100

    Update install script error messages

--- install.sh
+++ install.sh
@@ -151,15 +151,17 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-curl -fsSL "https://raw.githubusercontent.com/matiboux/dockerc/$INSTALL_TAG/dockerc" -o "$INSTALL_DIR/dockerc"
-if [ $? -ne 0 ]; then
-	echo 'Error: DockerC installation failed.' >&2
+curl -fsSL "https://raw.githubusercontent.com/matiboux/dockerc/$INSTALL_TAG/dockerc" 2>/dev/null \
+	-o "$INSTALL_DIR/dockerc"
+CURL_CODE=$?
+if [ "$CURL_CODE" -ne 0 ]; then
+	echo "Error: Failed to download DockerC ($INSTALL_TAG) in '$INSTALL_DIR' (curl error $CURL_CODE)." >&2
 	exit 1
 fi
 
 chmod +x "$INSTALL_DIR/dockerc"
 if [ $? -ne 0 ]; then
-	echo 'Error: Failed to add execute permission.' >&2
+	echo 'Error: Failed to add execute permission to DockerC.' >&2
 	exit 1
 fi
 
