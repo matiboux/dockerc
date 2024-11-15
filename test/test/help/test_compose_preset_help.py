3b279b8fdb945c0ad065d082cc97f736c66188cd
Matiboux <matiboux@gmail.com>
2024-11-15 12:53:04 +0100

    Fix presets naming

--- test/test/help/test_compose_preset_help.py
+++ test/test/help/test_compose_preset_help.py
@@ -5,7 +5,7 @@ from test.src.TestDirContext import TestDirContext
 def test_compose_preset_help(file = __file__):
     with TestDirContext(file) as ctx:
         dockerc = ctx.run_dockerc(
-            '@',
+            '-', '@',
         )
         dockerc.assert_context_ok(
             re.compile('^Usage:'),
